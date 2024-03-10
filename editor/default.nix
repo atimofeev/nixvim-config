186cfff2494dfd26832763a64a66aab31c3eb8b2
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-03-11 00:54:15 +0400

    mini.surround: fix KB collision with leap.nvim

--- editor/default.nix
+++ editor/default.nix
@@ -9,10 +9,19 @@
 
     comment-nvim.enable = true;
 
-    # TODO: learn KBs: https://github.com/echasnovski/mini.surround
     mini = {
       enable = true;
-      modules.surround = { };
+      modules.surround = {
+        mappings = { # fix KB collision with leap.nvim
+          add = "gza"; # Add surrounding in Normal and Visual modes
+          delete = "gzd"; # Delete surrounding
+          find = "gzf"; # Find surrounding (to the right)
+          find_left = "gzF"; # Find surrounding (to the left)
+          highlight = "gzh"; # Highlight surrounding
+          replace = "gzr"; # Replace surrounding
+          update_n_lines = "gzn"; # Update `n_lines`
+        };
+      };
     };
 
     # TODO: setup these plugins
