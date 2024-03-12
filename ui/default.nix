90eead35aa29c7e0b223b7444ef0a6e1820ee1e1
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-03-12 14:04:38 +0400

    nvim-tree: update git-related settings

--- ui/default.nix
+++ ui/default.nix
@@ -32,6 +32,16 @@
       respectBufCwd = true;
       updateFocusedFile.enable = true;
       updateFocusedFile.updateRoot = true;
+      git.ignore = false;
+      filters = {
+        custom = [ ".git" ];
+        exclude = [ ".gitignore" ".github" ];
+      };
+      renderer = {
+        highlightGit = true;
+        icons.gitPlacement = "after";
+        icons.glyphs.git.unstaged = "•";
+      };
       # TODO: implement dynamic sizing of float window
       # https://www.reddit.com/r/neovim/comments/13u9okq/nvimtree_vs_neotree/
       # https://github.com/MarioCarrion/videos/blob/269956e913b76e6bb4ed790e4b5d25255cb1db4f/2023/01/nvim/lua/plugins/nvim-tree.lua
