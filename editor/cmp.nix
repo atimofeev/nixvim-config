210cc8dd5c98a6a67c3ed9bdd614dfb47ee56973
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-03-17 10:29:42 +0400

    cmp: +luasnip; +friendly-snippets; nvim-cmp conf

--- editor/cmp.nix
+++ editor/cmp.nix
@@ -2,24 +2,57 @@
   # TODO: add sources: https://github.com/hrsh7th/nvim-cmp/wiki/List-of-sources
   plugins = {
 
+    luasnip = {
+      enable = true;
+      extraConfig = {
+        history = true;
+        updateevents = "TextChanged,TextChangedI";
+      };
+    };
+
+    friendly-snippets.enable = true;
+
+    cmp-cmdline.enable = true;
+
     nvim-cmp = {
       enable = true;
       autoEnableSources = true;
-      sources =
-        [{ name = "nvim_lsp"; } { name = "path"; } { name = "buffer"; }];
+      sources = [
+        { name = "nvim_lsp"; }
+        { name = "luasnip"; }
+        { name = "buffer"; }
+        { name = "nvim_lua"; }
+        { name = "path"; }
+        { name = "calc"; }
+        { name = "emoji"; }
+      ];
+
       mapping = {
+        "<C-Space>" = "cmp.mapping.complete()";
+        "<C-e>" = "cmp.mapping.close()";
         "<CR>" = "cmp.mapping.confirm({select=true})";
         "<Tab>" = {
           action = ''
             function(fallback)
               if cmp.visible() then
                 cmp.select_next_item()
-              elseif luasnip.expandable() then
-                luasnip.expand()
-              elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-              elseif check_backspace() then
+              elseif require("luasnip").expand_or_jumpable() then
+                vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+              else
                 fallback()
+              end
+            end 
+          '';
+          modes = [ "i" "s" ];
+        };
+
+        "<S-Tab>" = {
+          action = ''
+            function(fallback)
+              if cmp.visible() then
+                cmp.select_prev_item()
+              elseif require("luasnip").jumpable(-1) then
+                vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
               else
                 fallback()
               end
@@ -29,6 +62,21 @@
         };
       };
     };
-
   };
+
+  extraConfigLua = ''
+    local cmp = require('cmp')
+    cmp.setup.cmdline({ '/', '?' }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = { { name = 'buffer' } }
+    })
+
+    cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = { { name = 'path' }, {
+            name = 'cmdline',
+            option = { ignore_cmds = { 'Man', '!' } },
+        } },
+    })
+  '';
 }
