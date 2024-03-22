6b6072d7548233caa1db4768d5c02e6113d82005
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-03-22 11:39:36 +0400

    lsp: +trouble

--- lsp/default.nix
+++ lsp/default.nix
@@ -1,28 +1,43 @@
 { ... }: {
-  # TODO: remove bg from DiagnosticUnderlineError and other related HL groups
   imports = [ ./none-ls.nix ];
 
-  plugins.lsp = {
-    enable = true;
-    servers = {
-      # langs
-      lua-ls.enable = true;
-      pylsp.enable = true;
-      ruff-lsp.enable = true;
-      gopls.enable = true;
-      bashls.enable = true;
-      # nil_ls.enable = true;
-      nixd.enable = true;
-      # devops
-      ansiblels.enable = true;
-      dockerls.enable = true;
-      # docker_compose_language_service.enable = true;
-      # groovyls.enable = true;
-      helm-ls.enable = true;
-      jsonls.enable = true;
-      # powershell_es.enable = true;
-      terraformls.enable = true;
-      yamlls.enable = true;
+  plugins = {
+
+    lsp = {
+      enable = true;
+      servers = {
+        # langs
+        lua-ls.enable = true;
+        pylsp.enable = true;
+        ruff-lsp.enable = true;
+        gopls.enable = true;
+        bashls.enable = true;
+        # nil_ls.enable = true;
+        nixd.enable = true;
+        # devops
+        ansiblels.enable = true;
+        dockerls.enable = true;
+        # docker_compose_language_service.enable = true;
+        # groovyls.enable = true;
+        helm-ls.enable = true;
+        jsonls.enable = true;
+        # powershell_es.enable = true;
+        terraformls.enable = true;
+        yamlls.enable = true;
+      };
+    };
+
+    trouble = {
+      enable = true;
+      useDiagnosticSigns = true;
+      # settings.use_diagnostic_signs = true;
     };
   };
+
+  keymaps = [{
+    mode = "n";
+    key = "<leader>d";
+    action = "<cmd> TroubleToggle <cr>";
+    options.desc = "Diagnostics";
+  }];
 }
