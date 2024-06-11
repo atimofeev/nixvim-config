2e5bbc88738b0a3e1bea18912eb16f950a78ce9b
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-06-11 12:51:58 +0400

    editor/cmp: fix cmp behavior on <CR> with no selection

--- editor/cmp.nix
+++ editor/cmp.nix
@@ -17,7 +17,6 @@ _: {
       enable = true;
       autoEnableSources = true;
       settings = {
-        preselect = "cmp.PreselectMode.None";
         snippet.expand =
           "function(args) require('luasnip').lsp_expand(args.body) end";
         window.completion = {
@@ -40,7 +39,7 @@ _: {
           "<C-d>" = "cmp.mapping.scroll_docs(-4)";
           "<C-e>" = "cmp.mapping.close()";
           "<C-f>" = "cmp.mapping.scroll_docs(4)";
-          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<CR>" = "cmp.mapping.confirm()";
           "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
           "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
         };
