be197cf9ca4ffa5efa5287ecb1519629ef8b34ea
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-06-18 12:44:14 +0400

    ui: +oil

---
+++ ui/oil.nix
@@ -0,0 +1,23 @@
+_: {
+  plugins = {
+    oil = {
+      enable = true;
+      settings = {
+        default_file_explorer = true;
+        delete_to_trash = true;
+        skip_confirm_for_simple_edits = true;
+        view_options = {
+          show_hidden = true;
+          natural_order = true;
+          is_always_hidden = # lua
+            ''
+              function(name, _)
+                return name == '..' or name == '.git'
+              end
+            '';
+        };
+        win_options = { wrap = true; };
+      };
+    };
+  };
+}
