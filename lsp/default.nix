c13e6aed92f42d9174b27fbd903213e802e6b520
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-07-06 13:35:31 +0400

    lsp: +borders for hover & signature_help

--- lsp/default.nix
+++ lsp/default.nix
@@ -35,6 +35,11 @@ _: {
     command = "setlocal wrap";
   }];
 
+  extraConfigLua = # lua
+    ''
+      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with( vim.lsp.handlers.hover, { border = "rounded" })
+      vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })'';
+
   keymaps = [
     {
       mode = "n";
