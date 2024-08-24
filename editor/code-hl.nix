85b332ba4d491b9e574c9c3ca38a7908d8d489b5
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-08-24 16:09:45 +0400

    editor/code-hl: +otter-nvim

--- editor/code-hl.nix
+++ editor/code-hl.nix
@@ -114,10 +114,23 @@
         hash = "sha256-txhRGtriIHg8oTRIUpn37OVlZtBJwPeOJQoYBFIu6JE=";
       };
     };
-  in [{ plugin = jinja.vim; }];
-  autoCmd = [{
-    event = [ "BufEnter" ];
-    pattern = [ "*.j2" ];
-    command = "TSBufDisable highlight";
-  }];
+  in [ { plugin = jinja.vim; } { plugin = pkgs.vimPlugins.otter-nvim; } ];
+  autoCmd = [
+    {
+      event = [ "BufEnter" ];
+      pattern = [ "*.j2" "*.tmpl" ];
+      command = "TSBufDisable highlight | LspStop";
+    }
+    {
+      event = [ "BufEnter" ];
+      pattern = [ "*.nix" ];
+      command = # lua
+        ''
+          lua require("otter").activate({"python", "lua", "bash"}, true, true, nil)'';
+    }
+  ];
+
+  plugins.cmp.settings.sources = [{ name = "otter"; }];
+
+  # NOTE: otter.nvim: https://github.com/nix-community/nixvim/pull/1768
 }
