a4aa2bab370fb73afbba51707ea1be2295d9c717
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-09-10 09:59:50 +0400

    +autocmd: +`wincmd =` on `VimResized`

---
+++ nvim/autocmd.nix
@@ -0,0 +1,27 @@
+_: {
+
+  extraConfigLua = # lua
+    ''
+      vim.api.nvim_create_autocmd('TextYankPost', {
+        desc = 'Highlight when yanking (copying) text',
+        callback = function()
+          vim.highlight.on_yank({higroup='Search', timeout=300})
+        end
+      })
+
+      vim.api.nvim_create_autocmd('TermClose', {
+        desc = 'Close terminal if its process has ended',
+        callback = function()
+           vim.cmd('bdelete')
+        end
+      })
+
+      vim.api.nvim_create_autocmd('VimResized', {
+        desc = 'Equalize windows on vim resize',
+        callback = function()
+           vim.cmd('wincmd =')
+        end
+      })
+    '';
+
+}
