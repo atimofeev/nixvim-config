a19729755cd0c5ba77a93f23c945a6f56ba21fb6
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-09-24 10:02:51 +0400

    plugins/git/gitlinker: rewrite

---
+++ nvim/plugins/git/gitlinker.nix
@@ -0,0 +1,34 @@
+{ pkgs, ... }: {
+
+  extraPlugins = let
+    gitlinker-nvim = pkgs.vimUtils.buildVimPlugin {
+      name = "gitlinker.nvim";
+      src = pkgs.fetchFromGitHub {
+        owner = "linrongbin16";
+        repo = "gitlinker.nvim";
+        rev = "542f51784f20107ef9ecdadc47825204837efed5";
+        hash = "sha256-OnlJf31dTzLOJ1tlDKH7slPnQGMZUloavEAtd/FxK0U=";
+      };
+    };
+  in [{ plugin = gitlinker-nvim; }];
+
+  extraConfigLua = # lua
+    ''
+      require('gitlinker').setup({
+        message = false,
+        highlight_duration = 0,
+        router = {
+          browse = {
+            ["^git%..*%.com"] = require('gitlinker.routers').gitlab_browse,
+          },
+          blame = {
+            ["^git%..*%.com"] = require('gitlinker.routers').gitlab_blame,
+          },
+        },
+      })
+
+      vim.keymap.set({'n','v'},'gC','<Cmd>GitLink!browse<CR>',{noremap = true, silent = true, desc = 'Open git code'})
+      vim.keymap.set({'n','v'},'gB','<Cmd>GitLink!blame<CR>',{noremap = true, silent = true, desc = 'Open git blame'})
+    '';
+
+}
