37b909d576dc6c8b55b7248617c302d606b9d7f1
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-03-12 14:07:29 +0100

    flake: simplify code

--- flake.nix
+++ flake.nix
@@ -1,11 +1,10 @@
 {
-  description = "A NeoVim configuration system for NixOS";
+  description = "A NeoVim configuration in Nix DSL";
 
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
 
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    # nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
 
     nixvim = {
       url = "github:nix-community/nixvim";
@@ -13,33 +12,22 @@
     };
   };
 
-  outputs = { nixpkgs, nixvim, flake-parts, ... }@inputs:
-    let config = import ./nvim; # nixvim config file
-    in flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs = { flake-parts, nixpkgs, nixvim, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
       systems =
         [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
-      perSystem = { pkgs, system, ... }:
-        let
-          nvim = nixvim.legacyPackages.${system}.makeNixvimWithModule {
-            inherit pkgs;
-            module = config;
-          };
-        in {
-          _module.args.pkgs = import nixpkgs {
-            inherit system;
-            config.allowUnfree = true;
-            overlays = [
-              (final: _prev: {
-                stable = import inputs.nixpkgs-stable {
-                  inherit (final) system;
-                  config.allowUnfree = true;
-                };
-              })
-            ];
-          };
-
-          packages.default = nvim; # 'nix run .' to start nixvim
+      perSystem = { pkgs, system, ... }: {
+        _module.args.pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
         };
+
+        packages.default =
+          nixvim.legacyPackages.${system}.makeNixvimWithModule {
+            inherit pkgs;
+            module = import ./nvim;
+          };
+      };
     };
 }
