7c549fc3a971c5bb2d5a58969a7a3cdc018b2701
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-03-26 15:58:06 +0100

    nixd: attempt to configure nixvim flake completion

--- nvim/plugins/lsp/nixd.nix
+++ nvim/plugins/lsp/nixd.nix
@@ -1,14 +1,18 @@
-_:
-let flake = ''(builtins.getFlake "/home/atimofeev/repos/nixos-config")'';
+{ system, ... }:
+let
+  nixos-flake = ''(builtins.getFlake "/home/atimofeev/repos/nixos-config")'';
+  nixvim-flake = ''(builtins.getFlake "/home/atimofeev/repos/nixvim-config")'';
 in {
   plugins.lsp.servers.nixd = {
     enable = true;
     settings = {
-      nixpkgs.expr = "import ${flake}.inputs.nixpkgs {}";
+      nixpkgs.expr = "import ${nixos-flake}.inputs.nixpkgs {}";
       options = rec {
-        nixos.expr = "${flake}.nixosConfigurations.default.options";
+        nixos.expr = "${nixos-flake}.nixosConfigurations.default.options";
         home-manager.expr =
           "${nixos.expr}.home-manager.users.type.getSubOptions []";
+        # NOTE: how do I pass current system here?
+        # nixvim.expr = "${nixvim-flake}.packages.${system}.default.options";
         # FIX:
         # nixvim.expr =
         #   ''"(${home-manager.expr}).programs.nixvim.type.getSubOptions []"'';
