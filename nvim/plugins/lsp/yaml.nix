0c150acb4eab2673a8108650cae13802c4732f43
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-03-26 15:08:49 +0100

    yaml-companion: bump revision

--- nvim/plugins/lsp/yaml.nix
+++ nvim/plugins/lsp/yaml.nix
@@ -1,4 +1,5 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
 
   # extraPlugins = let
   #   yaml-companion = pkgs.vimUtils.buildVimPlugin {
@@ -6,13 +7,13 @@
   #     src = pkgs.fetchFromGitHub {
   #       owner = "someone-stole-my-name";
   #       repo = "yaml-companion.nvim";
-  #       rev = "4de1e1546abc461f62dee02fcac6a02debd6eb9e";
-  #       hash = "sha256-BmX7hyiIMQfcoUl09Y794HrSDq+cj93T+Z5u3e5wqLc=";
+  #       rev = "131b0d67bd2e0f1a02e0daf2f3460482221ce3c0";
+  #       hash = "sha256-49498W7Hadju54Vi9oOnlNouujhnS8hLQ7ji/3cvIZg=";
   #     };
   #   };
   # in [{ plugin = yaml-companion; }];
   #
-  # extraConfigLua = # lua
+  # extraConfigLuaPost = # lua
   #   ''
   #     local cfg = require("yaml-companion").setup(opts)
   #     require("lspconfig")["yamlls"].setup(cfg)
