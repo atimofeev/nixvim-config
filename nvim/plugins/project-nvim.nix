8273832ec11f72d94c4cc8258274b7d445e7d58d
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-05-05 11:53:55 +0200

    project-nvim: fixed automatic dir change

--- nvim/plugins/project-nvim.nix
+++ nvim/plugins/project-nvim.nix
@@ -3,9 +3,14 @@ _: {
   plugins.project-nvim = {
     enable = true;
     settings = {
-      patterns = [ ".git" ".obsidian" ".terraform" ".terraform.lock.hcl" ];
-      ignore_lsp = [ "null-ls" "dockerls" "terraformls" "helm_ls" ];
+      detection_methods = [ "pattern" ];
+      # ignore_lsp = [ "null-ls" "dockerls" "terraformls" "helm_ls" ];
+      manual_mode = false;
+      patterns =
+        [ ".git" ".obsidian" ".terraform" ".terraform.lock.hcl" "Chart.yaml" ];
+      scope_chdir = "global";
       show_hidden = true;
+      silent_chdir = true;
     };
   };
 
