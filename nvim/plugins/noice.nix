3e8960fa3a0a627a7e88e2a40bd658fd271ee933
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-05-20 12:55:00 +0200

    fix(noice): fixed noice routes, added more rules

--- nvim/plugins/noice.nix
+++ nvim/plugins/noice.nix
@@ -3,16 +3,37 @@
   plugins.noice = {
     enable = true;
     settings = {
-      routes = [{
-        filter = [{
-          event = "msg_show";
-          any = [
-            { find = "%d fewer lines"; }
-            { find = "%d more lines"; }
-            { find = "%d lines yanked"; }
-          ];
-        }];
-      }];
+      presets = { lsp_doc_border = true; };
+      routes = [
+        # NOTE: show macro recording notification
+        {
+          filter.any = [{
+            event = "msg_showmode";
+            find = "recording";
+          }];
+          view = "notify";
+        }
+        # NOTE: hide useless notifications
+        {
+          filter = {
+            event = "msg_show";
+            any = [
+              { find = "%d fewer lines"; }
+              { find = "%d more lines"; }
+              { find = "%d lines yanked"; }
+            ];
+          };
+          opts.skip = true;
+        }
+        # NOTE: hide useless notifications
+        {
+          filter = {
+            event = "notify";
+            find = "No information available";
+          };
+          opts.skip = true;
+        }
+      ];
     };
   };
 
