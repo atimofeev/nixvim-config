9820af555eca4c699210e1264f9ba7fecad2d0f6
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2026-03-10 21:51:42 +0100

    chore(otter): removed redundant autoCmd

--- nvim/plugins/lsp/otter-nvim.nix
+++ nvim/plugins/lsp/otter-nvim.nix
@@ -1,12 +1,5 @@
 {
 
   plugins.otter.enable = true;
-  autoCmd = [
-    {
-      event = [ "BufEnter" ];
-      pattern = [ "*.nix" ];
-      command = ''lua require("otter").activate({"python", "lua", "bash"}, true, true, nil)'';
-    }
-  ];
 
 }
