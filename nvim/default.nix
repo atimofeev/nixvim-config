2536bf67e80c78dc891a2024a523f87007c48b37
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2026-03-10 20:04:18 +0100

    feat(plugins): implemented recursive file sourcing

--- nvim/default.nix
+++ nvim/default.nix
@@ -1,12 +1,22 @@
+{ lib, ... }:
 {
 
   imports = [
-    ./plugins
     ./autocmd.nix
     ./filetype.nix
     ./keymap.nix
     ./options.nix
-  ];
+  ]
+  ++ lib.concatMap (
+    dir:
+    lib.pipe (builtins.readDir dir) [
+      (lib.filterAttrs (path: _kind: !lib.hasPrefix "_" path))
+      (lib.filterAttrs (
+        _path: kind: kind == "directory" || (kind == "regular" && lib.hasSuffix ".nix" _path)
+      ))
+      (lib.mapAttrsToList (path: _kind: lib.path.append dir path))
+    ]
+  ) [ ./plugins ];
 
   clipboard = {
     providers.xclip.enable = true;
