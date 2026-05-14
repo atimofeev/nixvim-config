8445443ee7450d7d71d23dbbb184e493748e59a0
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2026-05-15 00:29:29 +0200

    chore(lsp): replaced deprecated lsp config with modern one

--- nvim/plugins/lsp/default.nix
+++ nvim/plugins/lsp/default.nix
@@ -67,17 +67,24 @@
 
   extraConfigLua = # lua
     ''
-      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
-      vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+      vim.lsp.config('*', {
+        handlers = {
+          ['textDocument/hover'] = { border = 'rounded' },
+          ['textDocument/signatureHelp'] = { border = 'rounded' },
+        },
+      })
 
-      local signs = { Error = "", Warn = "", Hint = "", Info = "" }
-      for type, icon in pairs(signs) do
-        local hl = "DiagnosticSign" .. type
-        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-      end
-
+      
       vim.diagnostic.config {
         virtual_text = { prefix = "" },
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = "",
+            [vim.diagnostic.severity.WARN] = "",
+            [vim.diagnostic.severity.INFO] = "",
+            [vim.diagnostic.severity.HINT] = "",
+          }
+        }
       }
 
       map("n", "<leader>lr", "<Cmd>lua vim.lsp.buf.rename()<CR>", { desc = "LSP rename" })
