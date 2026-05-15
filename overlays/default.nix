2aa09a7861dae772385b0658dcd459a48263b6a8
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2026-05-15 16:18:34 +0200

    feat(overlays): removed ansible-language-server pin to old version
    this package has returned to upstram, now building from vscode-ansible monorepo
    
    closes #18

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,13 +1,4 @@
 (final: prev: {
-  ansible-language-server =
-    let
-      ansiblels_1_2_1 = import (builtins.fetchTarball {
-        url = "https://github.com/NixOS/nixpkgs/archive/4910b0b71543d8c62f2eaf7cf52f6f9d8ae7ce62.tar.gz";
-        sha256 = "sha256:09j62ibsiifwv4i6sbr69zvw7qy1r5wn7in82pnkga1lhaf4baaw";
-      }) { system = final.stdenv.hostPlatform.system; };
-    in
-    ansiblels_1_2_1.ansible-language-server;
-
   # toggleterm.nvim: PR #670 - conditional_auto_scroll
   # Pauses autoscroll when scrolled up; resumes when cursor at bottom
   # Remove when PR merges and lands in nixpkgs
